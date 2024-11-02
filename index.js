27adc0aeca582affcc63c68a51d689b1a0dc1a6d
Franco Pereyra <francopereyra325@gmail.com>
2024-11-02 16:27:06 -0300

    agregue los middlewares, un archivo index.html y la carpeta publica

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
 
-console.log('hola mundo')
+const app = express()
+
+app.set('port', process.env.PORT || 4000)
+app.listen(app.get('port'), () => {
+    console.info('Estoy escuchando el puerto '+ app.get('port'))
+})
+
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+// http://localhost:4000/prueba
+app.get('/prueba', (req, res)=>{
+    console.log('alguien hizo una solicitud get a la ruta de prueba')
+    res.send('Hola mundo desde el Backend')
+})
