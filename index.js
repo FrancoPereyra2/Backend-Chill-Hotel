c1885f3c61fccc5b79cab9346d71925b93c766f1
Franco Pereyra <francopereyra325@gmail.com>
2024-11-02 16:40:53 -0300

    agregue el index.html como archivo estatico

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
 const app = express()
 
@@ -13,6 +15,9 @@ app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+app.use(express.static(path.join(__dirname, '/public')))
 
 // http://localhost:4000/prueba
 app.get('/prueba', (req, res)=>{
