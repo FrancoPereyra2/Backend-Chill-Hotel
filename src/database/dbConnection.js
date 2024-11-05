1fe803b9c7cc8d34377a0e70420520443467ff7b
axel gonzalez <ag180066@gmail.com>
2024-11-05 03:38:52 -0300

    se agrego controllers y se arreglo problemas del package json

--- src/database/dbConnection.js
+++ src/database/dbConnection.js
@@ -4,7 +4,7 @@ const mongoDB = process.env.MONGODB;
 
 mongoose.connect(mongoDB)
 
-const conexion = mongoose.connection;
+const conexion = mongoose.connection
 
 conexion.once('open', ()=>{
     console.info('BD Conectada')
